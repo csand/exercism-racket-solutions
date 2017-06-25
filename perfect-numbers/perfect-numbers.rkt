ec0f2f06c81e85a4fb251d0e981085b787d50f49
csand <sam@csand.net>
2017-06-25 18:53:43 -0400

    Iteration 2 of perfect numbers
    - More idiomatic `perfect?` predicate
    - Replace `in-range` with `range` so it's not a stream

--- perfect-numbers/perfect-numbers.rkt
+++ perfect-numbers/perfect-numbers.rkt
@@ -2,10 +2,10 @@
 (require math/base
          math/number-theory)
 
-(define (is-perfect? n)
+(define (perfect? n)
   (equal? n (- (sum (divisors n)) n)))
 
 (define (perfect-numbers end)
-  (filter is-perfect? (stream->list (in-range 1 (add1 end)) )))
+  (filter perfect? (range 1 (add1 end))))
 
 (provide perfect-numbers)
