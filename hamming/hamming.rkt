b8baf2eb0d8324ddeec0de2a685851934ac0f69f
csand <sam@csand.net>
2017-06-25 20:27:53 -0400

    Iteration 2 of hamming
    - Use `char=?` instead of `equal?`
    - Fix function body layout, use `unless`

--- hamming/hamming.rkt
+++ hamming/hamming.rkt
@@ -3,9 +3,8 @@
 (provide hamming-distance)
 
 (define (hamming-distance aseq bseq)
-  (if (equal? (string-length aseq) (string-length bseq))
-      (for/sum ([a aseq] [b bseq])
-        (if (equal? a b) 0 1))
-      (error "String lengths do not match.")
-      )
+  (unless (= (string-length aseq) (string-length bseq))
+    (error "String lengths do not match."))
+  (for/sum ([a aseq] [b bseq])
+    (if (char=? a b) 0 1))
   )
