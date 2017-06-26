b1d76c28aa4d0a08b081366087ab31553e131aba
csand <sam@csand.net>
2017-06-25 21:11:31 -0400

    Iteration 2 of scrabble score

--- scrabble-score/scrabble-score.rkt
+++ scrabble-score/scrabble-score.rkt
@@ -2,28 +2,35 @@
 
 (provide score)
 
-(define value1  (string->list "AEIOULNRST"))
-(define value2  (string->list "DG"))
-(define value3  (string->list "BCMP"))
-(define value4  (string->list "FHVWY"))
-(define value5  '(#\K))
-(define value8  (string->list "JX"))
-(define value10 (string->list "QZ"))
-
-(define (contains-char? chr l)
-  (ormap (curry char-ci=? chr) l))
-
 (define (score word)
-  (for/sum ([chr word])
-    (cond
-      [(contains-char? chr value1) 1]
-      [(contains-char? chr value2) 2]
-      [(contains-char? chr value3) 3]
-      [(contains-char? chr value4) 4]
-      [(contains-char? chr value5) 5]
-      [(contains-char? chr value8) 8]
-      [(contains-char? chr value10) 10]
-      [else 0]
-      )
-    )
-  )
+  (for/sum ([chr (string-upcase word)])
+    (hash-ref values chr 0)))
+
+(define values (hash
+                #\A 1
+                #\B 3
+                #\C 3
+                #\D 2
+                #\E 1
+                #\F 4
+                #\G 2
+                #\H 4
+                #\I 1
+                #\J 8
+                #\K 5
+                #\L 1
+                #\M 3
+                #\N 1
+                #\O 1
+                #\P 3
+                #\Q 10
+                #\R 1
+                #\S 1
+                #\T 1
+                #\U 1
+                #\V 4
+                #\W 4
+                #\X 8
+                #\Y 4
+                #\Z 10
+                ))
