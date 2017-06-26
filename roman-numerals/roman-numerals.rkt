7c863a1befe6a84f9c2486706d3f0cdc248af33c
csand <sam@csand.net>
2017-06-26 16:43:07 -0400

    Iteration 2 of roman numerals
    Fix code readability, use the threading macros package to clean up the steps in
    to-roman.

--- roman-numerals/roman-numerals.rkt
+++ roman-numerals/roman-numerals.rkt
@@ -2,32 +2,39 @@
 
 (provide to-roman)
 
+; Nothing to do with threads, macros for procedure application
+(require threading)
+
 (define (to-roman n)
-  (let* ([digits (string->list (number->string n))]
-         [numeralized (foldr (λ (x r)
-                               (let ([position (hash-ref positions (length r))]
-                                     [numeral (hash-ref numerals x)])
-                                 (cons (map (curry hash-ref position) numeral) r)))
-                             null
-                             digits)])
-    (string-join (map list->string numeralized) "")))
+  (~> n
+      number->list
+      ; Turn each digit into its roman characters for that digit's position
+      ; Position is determined by the current length of the accumulator list
+      (foldr (λ (x r) (cons (numeralize x (length r)) r)) null _)
+      (string-join "")
+      ))
 
-(define positions (hash
-                   0 (hash 'unit #\I 'five #\V 'ten #\X)
-                   1 (hash 'unit #\X 'five #\L 'ten #\C)
-                   2 (hash 'unit #\C 'five #\D 'ten #\M)
-                   3 (hash 'unit #\M)
-                   ))
+(define number->list (compose string->list number->string))
 
-(define numerals (hash
-                  #\0 null
-                  #\1 '(unit)
-                  #\2 '(unit unit)
-                  #\3 '(unit unit unit)
-                  #\4 '(unit five)
-                  #\5 '(five)
-                  #\6 '(five unit)
-                  #\7 '(five unit unit)
-                  #\8 '(five unit unit unit)
-                  #\9 '(unit ten)
-                  ))
+(define (numeralize n pos)
+  (let ([chars (chars-for-position pos)])
+    (list->string (map (curry hash-ref chars) (roman-repr n)))))
+
+(define/match (chars-for-position pos)
+  [(0) (hash 'one #\I 'five #\V 'ten #\X)]
+  [(1) (hash 'one #\X 'five #\L 'ten #\C)]
+  [(2) (hash 'one #\C 'five #\D 'ten #\M)]
+  [(3) (hash 'one #\M)])
+
+(define/match (roman-repr n)
+  [(#\0) null]
+  [(#\1) '(one)]
+  [(#\2) '(one one)]
+  [(#\3) '(one one one)]
+  [(#\4) '(one five)]
+  [(#\5) '(five)]
+  [(#\6) '(five one)]
+  [(#\7) '(five one one)]
+  [(#\8) '(five one one one)]
+  [(#\9) '(one ten)]
+  )
